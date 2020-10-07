f6bdf8f991c80ab601884d332d826c66fb0d9539
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2020-10-07 14:07:03 -0300

    Creación del proyecto.

---
+++ sistema de gestion - Server/src/sistema/de/gestion/server/SistemaDeGestionServer.java
@@ -0,0 +1,18 @@
+
+package sistema.de.gestion.server;
+
+/**
+ *
+ * @author bapo
+ */
+public class SistemaDeGestionServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}
